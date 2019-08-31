04d563bba02cbbebd570cc128bd9b226f0c9cfba
Azertray <47909744+Azertray@users.noreply.github.com>
2019-08-31 12:35:54 +0800

    updated for moved files

--- start.js
+++ start.js
@@ -2,6 +2,6 @@ let express = require("express");
 let app = express();
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static("/"));
+app.use(express.static("public"));
 
 app.listen(app.get('port'));
