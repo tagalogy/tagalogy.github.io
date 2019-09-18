178c193fb91a2a8c1ad257d605e4c07ad8690610
Azertray <47909744+Azertray@users.noreply.github.com>
2019-09-18 12:19:19 +0800

    added console logging

--- start.js
+++ start.js
@@ -1,10 +1,18 @@
-var express = require("express");
-var Terser = require("terser");
+"use strict";
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 5000
+
+// Start hosting static files
+console.log("Starting server");
+app.set("port", port);
 app.use(express.static("public", {
     maxAge: "0"
 }));
-
-app.listen(app.get('port'));
+app.listen(app.get('port'), function(error) {
+    if(error) throw error;
+    console.log("Server started! Listening at port " + port);
+});
+var option = {
+    module: true
+};
