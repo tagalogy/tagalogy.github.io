0062c11f6b0aa6dc0cdab14197e8ac407946862f
Azertray <47909744+Azertray@users.noreply.github.com>
2019-09-23 14:21:14 +0800

    Added button to dialog box

--- public/src/screen/dialog_box.js
+++ public/src/screen/dialog_box.js
@@ -4,6 +4,7 @@ import RoundedRectangle from "../2d/shape/rounded_rectangle.js";
 import Text from "../2d/shape/text.js";
 import SafeArea from "../2d/safearea.js";
 import {
+    sineIn,
     expoOut
 } from "../2d/easing.js";
 import {
@@ -12,7 +13,8 @@ import {
 import {
     colors
 } from "../asset.js";
-let dialogBox, msgBox;
+import timeout from "../timeout.js";
+let dialogBox, msgBox, cancelBox, okBox, cancelText, okText;
 let dialog = new Rectangle({
     x: 0,
     y: 0,
@@ -38,12 +40,54 @@ let dialog = new Rectangle({
                     font: "ComicNueue Angular",
                     size: 1 / 4,
             		color: colors.BLACK
+                }),
+                new Object2D({
+                    x: 0 / 8,
+                    y: 6 / 8,
+                    width: 8 / 8,
+                    height: 2 / 8,
+                    children: [
+                        cancelBox = new Object2D({
+                            x: 0,
+                            y: 0,
+                            width: 1 / 2,
+                            height: 1,
+                            child: cancelText = new Text({
+                                x: 0,
+                                y: 0,
+                                width: 1,
+                                height: 1,
+                                size: 4 / 10,
+                                color: colors.BLACK,
+                                font: "ComicNueue Angular",
+                                weight: "bold"
+                            })
+                        }),
+                        okBox = new RoundedRectangle({
+                            x: 1 / 2,
+                            y: 0,
+                            width: 1 / 2,
+                            height: 1,
+                            radius: 1 / 4,
+                            fill: colors.PH_BLUE,
+                            child: okText = new Text({
+                                x: 0,
+                                y: 0,
+                                width: 1,
+                                height: 1,
+                                size: 4 / 10,
+                                color: colors.WHITE,
+                                font: "ComicNueue Angular",
+                                weight: "bold"
+                            })
+                        })
+                    ]
                 })
             ]
         })
     })
 });
-export function popup(msg, ok, cancel) {
+export default async function popup(msg, ok, cancel) {
     dialog.addTo(scene);
     dialog.setOpacity(0);
     dialog.animateOpacity(3 / 4, 400);
@@ -60,4 +104,24 @@ export function popup(msg, ok, cancel) {
         height: 4 / 10,
     }, 400, expoOut);
     msgBox.content = msg;
+    okText.content = ok;
+    cancelText.content = cancel;
+    let value = await Promise.race([
+        overridePromise(okBox.once("interactup"), true),
+        overridePromise(cancelBox.once("interactup"), false)
+    ]);
+    dialog.animateOpacity(0, 200);
+    dialogBox.animateBound({
+        x: 1 / 6,
+        y: 10 / 10,
+        width: 4 / 6,
+        height: 4 / 10,
+    }, 200, sineIn);
+    await timeout(200);
+    dialog.remove();
+    return value;
+}
+async function overridePromise(promise, value) {
+    await promise;
+    return value;
 }
