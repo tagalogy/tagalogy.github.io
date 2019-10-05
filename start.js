989c8bd366ab1ffb2f29abdc5b7959e88cfc54d7
Azertray <47909744+Azertray@users.noreply.github.com>
2019-10-05 09:27:53 +0800

    Changed to modern js

--- start.js
+++ start.js
@@ -1,8 +1,7 @@
 "use strict";
-var express = require("express");
-var app = express();
-
-var port = process.env.PORT || 5000;
+let express = require("express");
+let app = express();
+let port = process.env.PORT || 5000;
 
 // Start hosting static files
 console.log("Starting server");
@@ -10,7 +9,7 @@ app.set("port", port);
 app.use(express.static("public", {
     maxAge: "0"
 }));
-app.listen(app.get('port'), function(error) {
+app.listen(app.get("port"), error => {
     if(error) throw error;
-    console.log("Server started! Listening at port " + port);
+    console.log(`Server started! Listening at port ${port}`);
 });
