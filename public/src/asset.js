54fddc7fdef89db003d82588a87f6ca7a3bddfbb
Azertray <47909744+Azertray@users.noreply.github.com>
2019-10-08 09:23:30 +0800

    Added words and difficulties

--- public/src/asset.js
+++ public/src/asset.js
@@ -1,18 +1,41 @@
 import {
 	loadImages
 } from "./2d/load_image.js";
+import {
+	getAll
+} from "./get_http.js";
 import Color from "./2d/color.js";
+export let words = {};
 export let images = {};
-export let assets = loadImages([
-	"./asset/title.png",
-	"./asset/title_dark.png",
-	"./asset/bulb.png",
-]);
-assets.then(rawImages => {
-	images.TITLE_PNG = rawImages[0];
-	images.TITLE_DARK_PNG = rawImages[1];
-	images.BULB_PNG = rawImages[2];
-});
+export let difficulties = {};
+const SPACES = /\s+/;
+async function loadAll() {
+	[
+		images.TITLE_PNG,
+		images.TITLE_DARK_PNG,
+		images.BULB_PNG
+	] = await loadImages(`
+		/asset/title.png
+		/asset/title_dark.png
+		/asset/bulb.png
+	`.trim().split(SPACES));
+	[
+		words.WORD_3,
+		words.WORD_4,
+		words.WORD_5,
+		words.WORD_6
+	] = (await getAll(`
+		/asset/word_3.txt
+		/asset/word_4.txt
+		/asset/word_5.txt
+		/asset/word_6.txt
+	`.trim().split(SPACES))).map(text => text.split(SPACES));
+	difficulties.EASY = [... words.WORD_3].sort();
+	difficulties.MEDIUM = [... words.WORD_3, ... words.WORD_4].sort();
+	difficulties.HARD = [... words.WORD_4, ... words.WORD_5].sort();
+	difficulties.VERY_HARD = [... words.WORD_5, ... words.WORD_6].sort();
+}
+export let assets = loadAll();
 export let colors = {
 	BACKGROUND: new Color("#fff"),
 	FOREGROUND: new Color("#000"),
