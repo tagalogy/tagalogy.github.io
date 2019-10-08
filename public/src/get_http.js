9ef021bc6ea1955607b84cb9b4572d36f89da33e
Azertray <47909744+Azertray@users.noreply.github.com>
2019-10-08 09:23:49 +0800

    Added http functionality

---
+++ public/src/get_http.js
@@ -0,0 +1,20 @@
+export function get(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest;
+        xhr.addEventListener("load", () => {
+            if(xhr.status === 200) {
+                resolve(xhr.responseText);
+            }else{
+                reject(xhr.status);
+            }
+        });
+        xhr.addEventListener("error", () => {
+            reject();
+        });
+        xhr.open("GET", url);
+        xhr.send();
+    });
+}
+export function getAll(urls) {
+	return Promise.all(urls.map(url => get(url)));
+}
