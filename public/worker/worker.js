de33ea7ed87a57a876a46c7dbfb1bde9ec132a6a
Azertray <47909744+Azertray@users.noreply.github.com>
2019-10-09 19:39:53 +0800

    hopefully fixed worker

--- public/worker/worker.js
+++ public/worker/worker.js
@@ -3,20 +3,24 @@ let path = `
     /
     /index.html
     /src_min/main.js
+
+    /asset/font.css
     /asset/comicnueue_angular/bold_italic.otf
     /asset/comicnueue_angular/bold.otf
     /asset/comicnueue_angular/italic.otf
     /asset/comicnueue_angular/light_italic.otf
     /asset/comicnueue_angular/light.otf
     /asset/comicnueue_angular/regular.otf
-    /asset/font.css
+
     /asset/bulb.png
     /asset/title.png
     /asset/title_dark.png
+
     /asset/word_3.txt
     /asset/word_4.txt
     /asset/word_5.txt
     /asset/word_6.txt
+
     /icon/16.png
     /icon/32.png
     /icon/64.png
@@ -25,28 +29,27 @@ let path = `
     /icon/1024.png
     /icon/2048.png
 `.trim().split(/\s+/);
+async function addAllCache() {
+    let cache = await caches.open(cacheName);
+    await cache.addAll(path);
+}
+async function deleteOldCache() {
+    for(let key of await caches.keys()) if(cacheName !== key) await caches.delete(key);
+}
+async function fetchAndCache(request) {
+    let cached = await caches.match(request);
+    if(cached) return cached;
+    let response = await fetch(request);
+    let cache = await caches.open(cacheName);
+    cache.put(request, response);
+    return response;
+}
 self.addEventListener("install", event => {
-    event.waitUntil(caches.open(cacheName).then(cache => {
-        return cache.addAll(path);
-    }));
+    event.waitUntil(addAllCache());
 });
 self.addEventListener("activate", event => {
-    event.waitUntil(caches.keys().then(keyList => {
-        return Promise.all(keyList.map(key => {
-            if (cacheName.indexOf(key) === -1) {
-                return caches.delete(key);
-            }
-        }));
-    }));
+    event.waitUntil(deleteOldCache());
 });
 self.addEventListener("fetch", event => {
-    event.respondWith(caches.match(event.request).then(cache => {
-        if (cache) return cache;
-        return fetch(event.request).then(response => {
-            return caches.open(cacheName).then(cache => {
-                cache.put(event.request, request.clone());
-                return response;
-            });
-        });
-    }));
+    event.respondWith(fetchAndCache(event.request));
 });
