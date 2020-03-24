9db97a69bf57548467c47fd02bd4d86ecdf46469
neverRare <35887047+neverRare@users.noreply.github.com>
2020-03-24 15:35:36 +0800

    added exit to pause screen

--- src/actions/pause.ts
+++ src/actions/pause.ts
@@ -1,7 +1,22 @@
-import {pauseScreen} from "../components/pause";
+import {pauseScreen, continuePlace, backPlace} from "../components/pause";
+import {overridePromise} from "../utils/override_promise";
+import {popup} from "./dialog_box";
 
-export async function pause(): Promise<void> {
+export async function pause(): Promise<boolean> {
     await pauseScreen.fadeIn();
-    await pauseScreen.once("interactup");
-    await pauseScreen.fadeOut();
+    while (true) {
+        const respondContinue = await Promise.race([
+            overridePromise(continuePlace.once("interactup"), true),
+            overridePromise(backPlace.once("interactup"), false),
+        ]);
+        if (respondContinue) {
+            await pauseScreen.fadeOut();
+            return true;
+        }
+        const confirmContinue = await popup("Sigurado kang gusto mong itigil ang laro at bumalik?", "Ituloy", "Itigil");
+        if (! confirmContinue) {
+            await pauseScreen.fadeOut();
+            return false;
+        }
+    }
 }
