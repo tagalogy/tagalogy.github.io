91bc3720d9cd6406e08c9aad69a949dfb3c48b26
neverRare <35887047+neverRare@users.noreply.github.com>
2020-05-24 09:16:25 +0800

    more format

--- rollup_config/watch.js
+++ rollup_config/watch.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "rollup-plugin-typescript2";
 
 export default {
     input: "src/main.ts",
--- start.js
+++ start.js
@@ -5,10 +5,12 @@ let port = process.env.PORT || 4000;
 
 console.log("Starting server");
 app.set("port", port);
-app.use(express.static("public", {
-    maxAge: "0",
-}));
+app.use(
+    express.static("public", {
+        maxAge: "0",
+    }),
+);
 app.listen(app.get("port"), error => {
-    if(error) throw error;
+    if (error) throw error;
     console.log(`Server started! Listening at port ${port}`);
 });
